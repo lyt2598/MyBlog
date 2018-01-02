99c5adb903da9cb15dd462b55f784bb6a479e969
lyt <lyt2598@qq.com>
2018-01-02 15:51:37 +0800

    change

--- project/MyBlog/src/main/java/com/liaoyingtai/blog/controller/learningNotes/LearningNotesController.java
+++ project/MyBlog/src/main/java/com/liaoyingtai/blog/controller/learningNotes/LearningNotesController.java
@@ -1,6 +1,5 @@
 package com.liaoyingtai.blog.controller.learningNotes;
 
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -8,17 +7,12 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 
 import com.liaoyingtai.blog.controller.exception.MyExceptionResolverResultPage;
-import com.liaoyingtai.blog.service.learningNotes.LearningNotesService;
 
 @Controller
 public class LearningNotesController extends MyExceptionResolverResultPage {
 
-	@Autowired
-	private LearningNotesService learningNotesService;
-
 	@RequestMapping(value = "learningNotesList/{userId}", method = { RequestMethod.GET })
-	public String learningNotesList(Model model,
-			@PathVariable("userId") String uid) throws Exception {
+	public String learningNotesList(Model model, @PathVariable("userId") String uid) throws Exception {
 		model.addAttribute("userId", uid);
 		return "learningNotes/learningNotesList";
 	}
